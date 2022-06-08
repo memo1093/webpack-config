f390e188fe3b3c415000294cc2b82673bdd4c90d
Mehmet Akyer <mehmetakyer006@gmail.com>
2022-06-08 10:34:50 +0300

    babel support added

--- src/index.js
+++ src/index.js
@@ -4,4 +4,11 @@ const elvenShieldRecipe ={
     refinedMoonstone:4
 }
 
+const elvenGauntletRecipe={
+    ...elvenShieldRecipe,
+    leather:1,
+    refinedMoonstone:4
+}
+
 console.log(elvenShieldRecipe)
+console.log(elvenGauntletRecipe)
