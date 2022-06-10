7cc3c2c484f856abbb3e912eaccece8259bf40f0
Mehmet Akyer <mehmetakyer006@gmail.com>
2022-06-10 12:38:13 +0300

    add css,sass,postcss, hotreloading and browserlists

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 let mode ="development"
+let target = "web"
 if (process.env.NODE_ENV==="production") {
     mode="production"
+    target="browserslist"
 }
 module.exports={
-    mode:mode,
+    mode,
+    target,
     module:{
         rules:[
+            {
+                test:/\.(s[ac]|c)ss$/i,
+                use:[MiniCssExtractPlugin.loader,"css-loader","postcss-loader","sass-loader"]
+            },
             {
                 test:/\.js$/,
                 exclude:/node_modules/,
@@ -15,9 +23,11 @@ module.exports={
             }
         ]
     },
+    plugins:[new MiniCssExtractPlugin()],
     devtool:"source-map",
     devServer:{
         static:"./dist",
-        open:true
-    }
+        open:true,
+        hot:true
+    },
 }
